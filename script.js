381fa5fd4e1d95f548a40ed6cd773d01ce71c2af
Lexi <alexisortiz473@gmail.com>
2023-01-30 22:00:29 -0500

    building menu popup

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ function showEmail() {
     popupMail.classList.toggle("show");
 }
 
-$(window).load(function () {
-    $(".btn-nav").on("click tap", function () {
-        $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
-        $(this).toggleClass("animated");
-    });
-});
+function showMenu() {
+    let popupMenu = document.getElementById("popMenu");
+    popupMenu.classList.toggle("show");
+}
